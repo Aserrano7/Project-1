3c436ef3802d4b9557b6b05ef44a24949a7838a7
awjcodes <146032624+awjcodes@users.noreply.github.com>
2023-11-14 19:40:40 -0800

    Choose your workout Selections and Workout Displayed Fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
 var apiKeyExcercise = "QzUuvPoamyqc5YGXvPJpCw==pAl5Dfme18ylrDTm";
 
 var saveBtn = document.getElementById('saveButton');
-saveBtn.addEventListener('click', function(event){
+saveBtn.addEventListener('click', function (event) {
     event.preventDefault();
     var type = document.getElementById('typeE');
     console.log(type.value);
-    var muscle =document.getElementById("muscleE");
+    var muscle = document.getElementById("muscleE");
     console.log(muscle.value);
-    var diff=document.getElementById("difficultyE")
+    var diff = document.getElementById("difficultyE")
     console.log(diff.value);
 
 
@@ -27,44 +27,40 @@ saveBtn.addEventListener('click', function(event){
     }
     console.log(userInput);
 
-
-if(type.value==='all' && muscle.value==='all' && diff.value==='all' ){
-
-var url = 'https://api.api-ninjas.com/v1/exercises?';
-fetch(url, {
-    method: 'GET',
-    headers: { 'X-Api-Key': apiKeyExcercise },
-})
-
-    .then(function (response) {
-        console.log(response);
-        return response.json();
+    var url = "https://api.api-ninjas.com/v1/exercises?&type=" + type.value + "&muscle=" + muscle.value + "&difficulty=" + diff.value + "&appid=" + apiKeyExcercise
+    fetch(url, {
+        method: 'GET',
+        headers: { 'X-Api-Key': apiKeyExcercise },
     })
-    .then(function (data) {
-        console.log(data);
 
-        for (let index = 0; index < data.length; index++) {
-            var name = document.createElement('div');
-            var muscle = document.createElement('div');
-            var type = document.createElement('div');
-            var equipment = document.createElement('div');
-            var instructions = document.createElement('div');
-            var button= document.createElement('button')
+        .then(function (response) {
+            console.log(response);
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+
+            for (let index = 0; index < data.length; index++) {
+                var name = document.createElement('div');
+                var muscle = document.createElement('div');
+                var type = document.createElement('div');
+                var equipment = document.createElement('div');
+                var instructions = document.createElement('div');
+                var button = document.createElement('button')
+
+                name.textContent = "name: " + data[index].name;
+                type.textContent = "type: " + data[index].type;
+                muscle.textContent = "muscle: " + data[index].muscle;
+                equipment.textContent = "equipment: " + data[index].equipment;
+                instructions.textContent = "instruction" + data[index].instructions;
+                button.textContent = "link to video";
+
+                var display = document.getElementById('description');
+                display.append(name, type, muscle, equipment, instructions, button);
 
 
-            name.textContent = "name: " + data[index].name;
-            type.textContent = "type: " + data[index].type;
-            muscle.textContent = "muscle: " + data[index].muscle;
-            equipment.textContent = "equipment: " + data[index].equipment;
-            instructions.textContent = "instruction" + data[index].instructions;
-            button.textContent="link to video";
-
-            var display = document.getElementById('description');
-            display.append(name, type, muscle, equipment, instructions,button);
+            }
 
 
-        }
-
-
-    }) 
-}
+        });
+})
