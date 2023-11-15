1de6f43531dc37ed36641ff2ae157f2cd3c755e7
ylf456 <145236890+ylf456@users.noreply.github.com>
2023-11-14 21:31:17 -0800

    changed value of all empty to an empty string, deleted the if conditions to set the all option value in js.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,16 @@
 var apiKeyExcercise = "QzUuvPoamyqc5YGXvPJpCw==pAl5Dfme18ylrDTm";
+var display = document.getElementById('display');
 
 var saveBtn = document.getElementById('saveButton');
-saveBtn.addEventListener('click', function(event){
+saveBtn.addEventListener('click', function (event) {
     event.preventDefault();
     var type = document.getElementById('typeE');
     console.log(type.value);
-    var muscle =document.getElementById("muscleE");
+    var muscle = document.getElementById("muscleE");
     console.log(muscle.value);
-    var diff=document.getElementById("difficultyE")
+    var diff = document.getElementById("difficultyE")
     console.log(diff.value);
-
-
-    if (type.value === "all") {
-        type.value = "";
-    }
-    if (muscle.value === 'all') {
-        muscle.value = '';
-    }
-    if (diff.value === 'all') {
-        diff.value = "";
-    }
+    //virtue object for debugging;
     var userInput = {
         type: type.value,
         muscle: muscle.value,
@@ -27,44 +18,40 @@ saveBtn.addEventListener('click', function(event){
     }
     console.log(userInput);
 
+    // clear the content before append new lists
+    display.innerHTML =""
 
-if(type.value==='all' && muscle.value==='all' && diff.value==='all' ){
+    var url = "https://api.api-ninjas.com/v1/exercises?&type=" + type.value + "&muscle=" + muscle.value + "&difficulty=" + diff.value + "&appid=" + apiKeyExcercise
+     
+    console.log("assembled Url is: " + url)
+    fetch(url, {
+            method: 'GET',
+            headers: { 'X-Api-Key': apiKeyExcercise },
+        })
+            .then(function (response) {
+                console.log(response);
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data);
 
-var url = 'https://api.api-ninjas.com/v1/exercises?';
-fetch(url, {
-    method: 'GET',
-    headers: { 'X-Api-Key': apiKeyExcercise },
-})
+                for (let index = 0; index < data.length; index++) {
+                    var name = document.createElement('div');
+                    var muscle = document.createElement('div');
+                    var type = document.createElement('div');
+                    var equipment = document.createElement('div');
+                    var instructions = document.createElement('div');
+                    var button = document.createElement('button')
+                    
+                    name.textContent = "name: " + data[index].name;
+                    type.textContent = "type: " + data[index].type;
+                    muscle.textContent = "muscle: " + data[index].muscle;
+                    equipment.textContent = "equipment: " + data[index].equipment;
+                    instructions.textContent = "instruction" + data[index].instructions;
+                    button.textContent = "link to video";
 
-    .then(function (response) {
-        console.log(response);
-        return response.json();
+                    var display = document.getElementById('display');
+                    display.append(name, type, muscle, equipment, instructions, button);
+                }
+            })
     })
-    .then(function (data) {
-        console.log(data);
-
-        for (let index = 0; index < data.length; index++) {
-            var name = document.createElement('div');
-            var muscle = document.createElement('div');
-            var type = document.createElement('div');
-            var equipment = document.createElement('div');
-            var instructions = document.createElement('div');
-            var button= document.createElement('button')
-
-
-            name.textContent = "name: " + data[index].name;
-            type.textContent = "type: " + data[index].type;
-            muscle.textContent = "muscle: " + data[index].muscle;
-            equipment.textContent = "equipment: " + data[index].equipment;
-            instructions.textContent = "instruction" + data[index].instructions;
-            button.textContent="link to video";
-
-            var display = document.getElementById('description');
-            display.append(name, type, muscle, equipment, instructions,button);
-
-
-        }
-
-
-    }) 
-}
