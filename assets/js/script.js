27a1ab590010ca9d6d85fde28c6d5f19f04cceb5
Aserrano7 <serranoandres022@gmail.com>
2023-11-15 21:03:04 -0800

    added fetch call for youtube api and promise all so that we can get the video link to be on all the butttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,43 @@
+
 var apiKeyExcercise = "QzUuvPoamyqc5YGXvPJpCw==pAl5Dfme18ylrDTm";
+var youtubeApiKey = 'AIzaSyDDyQK0OeYbA_DvFsDlnlUg46yBBYPS2P0';
+var urlArray = [];
 
 var saveBtn = document.getElementById('saveButton');
-saveBtn.addEventListener('click', function(event){
+saveBtn.addEventListener('click', function (event) {
     event.preventDefault();
     var type = document.getElementById('typeE');
-    console.log(type.value);
-    var muscle =document.getElementById("muscleE");
-    console.log(muscle.value);
-    var diff=document.getElementById("difficultyE")
-    console.log(diff.value);
+    var muscle = document.getElementById("muscleE");
+    var diff = document.getElementById("difficultyE");
 
-if(type.value==='all' && muscle.value==='all' && diff.value==='all' ){
+    if (type.value === "all") {
+        type.value = "";
+    }
+    if (muscle.value === 'all') {
+        muscle.value = '';
+    }
+    if (diff.value === 'all') {
+        diff.value = "";
+    }
 
-var url = 'https://api.api-ninjas.com/v1/exercises?';
-fetch(url, {
-    method: 'GET',
-    headers: { 'X-Api-Key': apiKeyExcercise },
-})
+    var userInput = {
+        type: type.value,
+        muscle: muscle.value,
+        difficulty: diff.value,
+    }
 
+    var url = 'https://api.api-ninjas.com/v1/exercises?type=' + userInput.type + '&muscle=' + userInput.muscle + "&difficulty=" + userInput.difficulty;
+
+    fetch(url, {
+        method: 'GET',
+        headers: { 'X-Api-Key': apiKeyExcercise },
+    })
     .then(function (response) {
-        console.log(response);
         return response.json();
     })
     .then(function (data) {
-        console.log(data);
+        var display = document.getElementById('display');
+        display.innerHTML = ''; // Clear previous results
 
         for (let index = 0; index < data.length; index++) {
             var name = document.createElement('div');
@@ -31,26 +45,42 @@ fetch(url, {
             var type = document.createElement('div');
             var equipment = document.createElement('div');
             var instructions = document.createElement('div');
-            var button= document.createElement('button')
-
-
+            var button = document.createElement('a'); // Change button to an anchor element
+            button.setAttribute('id', 'btn-' + index);
             name.textContent = "name: " + data[index].name;
             type.textContent = "type: " + data[index].type;
             muscle.textContent = "muscle: " + data[index].muscle;
             equipment.textContent = "equipment: " + data[index].equipment;
-            instructions.textContent = "instruction" + data[index].instructions;
-            button.textContent="link to video";
+            instructions.textContent = "instruction: " + data[index].instructions;
+            button.textContent = "Button";
+            display.append(name, type, muscle, equipment, instructions, button);
 
-            var display = document.getElementById('description');
-            display.append(name, type, muscle, equipment, instructions,button);
-
-
+            var url2 = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=' + data[index].name + '&type=video&key=' + youtubeApiKey;
+            urlArray.push(url2);
         }
 
+        var requests = urlArray.map(function(url) {
+            return fetch(url).then(response => response.json());
+        });
 
-    }) 
-}
+        Promise.all(requests).then(function (results) {
+            for (let index = 0; index < results.length; index++) {
+                var buttonElement = document.getElementById('btn-' + index);
+               
+                var item = results[index].items;
+                var videoId = item[0].id.videoId;
+                var videoUrl = 'https://www.youtube.com/watch?v=' + videoId;
+             
+                buttonElement.setAttribute("href", videoUrl);
+                buttonElement.setAttribute('target', '_blank');
+            }
+        });
+    })
+    .catch(function (error) {
+        console.error('Error:', error);
+    });
+});
 
 
-})
+
 
